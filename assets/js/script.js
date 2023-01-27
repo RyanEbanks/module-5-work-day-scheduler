1595abd128b10a944adb95e1f6d4027745578d2e
RyanEbanks <ryankebanks@gmail.com>
2023-01-27 15:47:53 -0500

    Added notes for myself, Got the functionality of the calendar past, present and future working

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,9 +15,69 @@ $(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+
+  // newTime = dayjs().hour(); // gets current hour
+  var idObject = {
+    9: "#hour-9",
+    10: "#hour-10",
+    11: "#hour-11",
+    12: "#hour-12",
+    13: "#hour-13",
+    14: "#hour-14",
+    15: "#hour-15",
+    16: "#hour-16",
+    17: "#hour-17"
+  }
+
+  newTime = dayjs().hour();
+  console.log(newTime);
+
+   for(var i = 9; i <= 17; i++) {
+
+     if(newTime === i) {
+       $(idObject[i]).removeClass("future");
+       $(idObject[i]).removeClass("past");
+       $(idObject[i]).addClass("present");
+     }
+     else if(newTime > i) {
+      $(idObject[i]).removeClass("future");
+      $(idObject[i]).removeClass("present");
+      $(idObject[i]).addClass("past");
+    } else {
+      $(idObject[i]).removeClass("present");
+      $(idObject[i]).removeClass("past");
+      $(idObject[i]).addClass("future");
+    }
+     console.log(idObject[i]);
+   }
+  //   console.log(idObject[i]);
+
+  // for(var i = 0; i < 13; i++) {
+  //   $("#hour-[i]").removeAttr('class', 'future')
+  //   $("#hour-11").attr('class', 'present');
+  // }
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+
+
+//Providing the current date to the header div
+var currentDay = dayjs().format("MMMM D, YYYY");
+$('#currentDay').text(currentDay);
+console.log(currentDay);
+
+
+/* 
+1. Time blocks for standard business hours ...........v
+2. Based on time using dayjs or setinterval the
+class past present or future will be appended to a div
+3. Past Present and future hold their own timeblock color
+4. Click event for timeblocks, I can enter event
+5. Save Button
+6. When save button is clicked text is saved in local storage
+7. When refreshed the saved events stay on the page
+*/
