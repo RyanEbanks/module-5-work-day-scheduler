766de23baca1a9c415c64a3fe691796989f79531
RyanEbanks <ryankebanks@gmail.com>
2023-01-31 15:18:53 -0500

    Added the finishing touches to the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 $(function () {
   //Storing the names of the ID's into objects to reference them later
   var idObject = {
@@ -14,41 +13,30 @@ $(function () {
   }
   newTime = dayjs().hour(); //Getting the current hour
 
-var idAndClass ={
-  9: "#hour-9 .description",
-  10: "#hour-10 .description",
-  11: "#hour-11 .description",
-  12: "#hour-12 .description",
-  13: "#hour-13 .description",
-  14: "#hour-14 .description",
-  15: "#hour-15 .description",
-  16: "#hour-16 .description",
-  17: "#hour-17 .description"
-}
-
-  $(".saveBtn").on("click", function() {
-    for(var i = 9; i <= 17; i++){
-      var descriptionText = $(idAndClass[i]).val();
-      console.log(descriptionText);
-      console.log(i);
-      if(descriptionText) {
-        localStorage.setItem(idObject[i], descriptionText);
-        console.log($(`#hour-${i} .description`));
+  //On click event the save button will execute the code inside
+  $(".saveBtn").on("click", function () {
+    for (var i = 9; i <= 17; i++) {
+      //Iterating through the hour id with the class description and getting the value
+      var descriptionText = $(`#hour-${i} .description`).val();
+      if (descriptionText) {
+      //Adding that current iteration to local storage
+        localStorage.setItem(`#hour-${i}`, descriptionText);
       }
-      }
-
+    }
   });
- 
-//This updates the past, present and future colors into divs based on the ID
-   for(var j = 9; j <= 17; j++) {
-       var textAreaStorage = localStorage.getItem(idObject[j]);
-        $($(`#hour-${j} .description`)).html(textAreaStorage);
-     if(newTime === j) {
-       $(idObject[j]).removeClass("future");
-       $(idObject[j]).removeClass("past");
-       $(idObject[j]).addClass("present");
-     }
-     else if(newTime > j) {
+
+  //This updates the past, present and future colors into divs based on the ID
+  for (var j = 9; j <= 17; j++) {
+    //Getting items from local storage by iterating the through id names
+    var textAreaStorage = localStorage.getItem(idObject[j]);
+    //Printing the text value from the local storage to the html
+    $($(`#hour-${j} .description`)).html(textAreaStorage);
+    if (newTime === j) {
+      $(idObject[j]).removeClass("future");
+      $(idObject[j]).removeClass("past");
+      $(idObject[j]).addClass("present");
+    }
+    else if (newTime > j) {
       $(idObject[j]).removeClass("future");
       $(idObject[j]).removeClass("present");
       $(idObject[j]).addClass("past");
@@ -57,7 +45,8 @@ var idAndClass ={
       $(idObject[j]).removeClass("past");
       $(idObject[j]).addClass("future");
     }
-   }
+  }
+  //Setting the time to the current day
   var currentDay = dayjs().format("MMMM D, YYYY");
   $('#currentDay').text(currentDay);
 });
